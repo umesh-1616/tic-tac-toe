a24794b7dd1241f70f15535ede9f26c8baae8c39
umesh-1616 <umeshmhanta27@gmail.com>
2025-06-30 10:07:34 +0530

    Initial commit

---
+++ src/Component/TicTacToe.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+function TicTacToe() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXTurn, setIsXTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const handleClick = (index) => {
+    console.log(index, "Click");
+    const newBoard = [...board];
+    newBoard[index] = isXTurn ? "X" : "O";
+    setBoard(newBoard);
+    setIsXTurn(!isXTurn);
+
+    const winnerCombination = checkWinner(newBoard);
+    if (winnerCombination) {
+      setWinner(newBoard[winnerCombination[0]]);
+    }
+  };
+
+  const checkWinner = (newBoard) => {
+    const combination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < combination.length; i++) {
+      const [a, b, c] = combination[i];
+      if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+        return combination[i];
+      }
+    }
+    return null;
+  };
+
+  return (
+    <>
+      <div className="board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+
+      {winner && <div>{winner} is Winner of this Game</div>}
+    </>
+  );
+}
+
+export default TicTacToe;
